4cba47566fc8290137c8961a488911cfc147a618
Juan David Ballesteros Perez <91141739+DavidBP014@users.noreply.github.com>
2024-04-21 12:25:36 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,34 +9,19 @@ import LoginPage from './components/LoginPage';
 import LocationPage from './components/LocationPage';
 
 const App = () => {
-    return ( <
-        Router >
-        <
-        Header / >
-        <
-        Switch >
-        <
-        Route path = "/"
-        exact component = { HomePage }
-        /> <
-        Route path = "/nosotros"
-        component = { AboutUsPage }
-        /> <
-        Route path = "/blog"
-        component = { BlogPage }
-        /> <
-        Route path = "/contacto"
-        component = { ContactPage }
-        /> <
-        Route path = "/login"
-        component = { LoginPage }
-        /> <
-        Route path = "/ubicacion"
-        component = { LocationPage }
-        /> <
-        /Switch> <
-        /Router>
-    );
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/nosotros" component={AboutUsPage} />
+        <Route path="/blog" component={BlogPage} />
+        <Route path="/contacto" component={ContactPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/ubicacion" component={LocationPage} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
